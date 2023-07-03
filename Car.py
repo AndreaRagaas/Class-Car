58d4462b73908ffc9b049db38a140a5d0655b34a
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-07-03 16:54:23 +0800

    Defining all of the objects for the Car

--- Car.py
+++ Car.py
@@ -1,2 +1,18 @@
 # Creating the class Car
 class Car:
+    # Defining all of the objects for the Car
+    def __init__(self, year_model, make):
+        self._year_model = year_model
+        self._make = make
+        self._speed = 0
+    
+    def accelerate(self):
+        self._speed += 5
+        print("Accelerating. Current speed:", self._speed)
+    
+    def brake(self):
+        self._speed -= 5
+        print("Braking. Current speed:", self._speed)
+    
+    def get_speed(self):
+        return self._speed
